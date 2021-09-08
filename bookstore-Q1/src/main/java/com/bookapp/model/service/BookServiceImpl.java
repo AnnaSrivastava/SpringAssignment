75ae0f86539decc5827ae18d5a0534722418642e
AnnaSrivastava <ananyavsrivastava@gmail.com>
2021-09-08 23:32:47 +0530

    third commit

---
+++ bookstore-Q1/src/main/java/com/bookapp/model/service/BookServiceImpl.java
@@ -0,0 +1,42 @@
+package com.bookapp.model.service;
+
+import java.util.List;
+
+import com.bookapp.model.dao.Book;
+import com.bookapp.model.dao.BookDao;
+
+public class BookServiceImpl implements BookService{
+
+	private BookDao bookDao;
+
+
+	public void setBookDao(BookDao bookDao) {
+		this.bookDao = bookDao;
+	}
+
+	@Override
+	public List<Book> getAllBooks() {
+		return bookDao.getAllBooks();
+	}
+
+	@Override
+	public Book addBook(Book book) {
+		return bookDao.addBook(book);
+	}
+
+	@Override
+	public void deleteBook(int id) {
+		bookDao.deleteBook(id);
+	}
+
+	@Override
+	public void updateBook(int id, Book book) {
+		bookDao.updateBook(id, book);
+	}
+
+	@Override
+	public Book getBookById(int id) {
+		return bookDao.getBookById(id);
+	}
+
+}
